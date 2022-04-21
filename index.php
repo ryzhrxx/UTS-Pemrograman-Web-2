7ffafae965b039d5b66a4f44260db9efb96e8b36
ryzhrxx <60371568+ryzhrxx@users.noreply.github.com>
2022-04-21 23:31:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Data Covid-19</a>
+    <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            DKI Jakarta
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">DKI Jakarta</a></li>
+            <li><a class="dropdown-item" href="#">Jawa Barat</a></li>
+            <li><a class="dropdown-item" href="#">Banten</a></li>
+            <li><a class="dropdown-item" href="#">Jawa Tengah</a></li>
+          </ul>
+        </li>
+  </div>
+</nav>
+<div class="container mt-5">
+    <div class="text-center">
+        <p>Data Pemantauan Covid19 Wilayah DKI Jakarta</p>
+        <p>Per 17 April 2022 14:08:16 ( Waktu dan Jam Sekarang )</p>
+        <p>Dimas alfian/181011401738</p>
+    </div>
+<table class="table  table-bordered mt-5">
+  <thead>
+    <tr>
+      <th scope="col">Positif</th>
+      <th scope="col">Dirawat</th>
+      <th scope="col">Sembuh</th>
+      <th scope="col">Meninggal</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1.245.653</td>
+      <td>411</td>
+      <td>1.227.793</td>
+      <td>1.227.793</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</html>
